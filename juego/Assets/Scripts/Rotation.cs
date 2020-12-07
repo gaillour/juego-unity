5ae3b5d56b3fd58b549657e433df436ff091d1df
gaillour <juangaillour.8@gmail.com>
2020-12-07 20:50:19 -0300

    rotacion agregada

---
+++ juego/Assets/Scripts/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public Transform sphere;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //direccion = objetivo - punto inicial
+        Vector3 direction = sphere.position - transform.position;
+        //Direccion en la que mirar
+        Quaternion look = Quaternion.LookRotation(direction);
+        //Hacer que la rotacion mire hacia la esfera y que se mueva mas lento con Time.deltaTime
+        transform.rotation = Quaternion.Slerp(transform.rotation, look, Time.deltaTime);
+    }
+}
